8c27fbc7bbc5547b2da38a0b829e8be147a39289
ALSchwalm <adamschwalm@gmail.com>
2013-12-12 22:47:25 -0600

    Updated treeGA

--- devolve/treeGA.d
+++ devolve/treeGA.d
@@ -2,48 +2,40 @@ module devolve.treeGA;
 import std.traits;
 import std.typetuple;
 
-struct Node(T) {
-    this(T t, string _name) {
-        val = t;
+class BaseNode(T) {
+    this(string _name) {
         name = _name;
     }
-    
-    T eval() {
-        return val;
-    }
 
-    T val;
-    uint children[ParameterTypeTuple!T.length];
+    abstract T eval();
     string name;
 }
 
+
+class Node(T) : BaseNode!(ReturnType!T) {
+    this(T t, string _name) {
+        super(_name);
+        val = t;
+    }
+    
+    override ReturnType!T eval() {
+        return val();
+    }
+
+    T val; 
+    BaseNode children[ParameterTypeTuple!T.length];
+}
+
 struct Tree(T) {
-    this(Node!T _root){
+    this(BaseNode!T _root){
         root = _root;
     }
-    T eval(){root.eval();}
+    T eval(){return root.eval();}
     
-    Node!T root;
+    BaseNode!T root;
 }
 
 struct TreeGA(T, uint PopSize) if (PopSize > 0) {
-    alias mutateFunc = void function(ref T);
-    alias fitnessFunc = double function(ref const T);
-    alias selectorFunc = void function(ref T[], fitnessFunc);
-    alias crossoverFunc = T function(ref const T, ref const T);
-    alias generatorFunc = T function();
-
-    this(fitnessFunc _fitness,
-         mutateFunc _mutate,
-         selectorFunc _selector,
-         crossoverFunc _crossover,
-         generatorFunc _generator) {
-        mutate=_mutate;
-        fitness=_fitness;
-        selector=_selector;
-        crossover=_crossover;
-        generator=_generator;
-    }
 
     void addNode(A)(A node)
         if (is(ReturnType!A == T) &&
@@ -51,13 +43,6 @@ struct TreeGA(T, uint PopSize) if (PopSize > 0) {
     {
         
     }
-    
-    immutable mutateFunc mutate;
-    immutable fitnessFunc fitness;
-    immutable selectorFunc selector;
-    immutable crossoverFunc crossover;
-    immutable generatorFunc generator;
 
     Tree!T functionTree;
 }
-
