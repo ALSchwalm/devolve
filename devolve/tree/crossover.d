2f34fcf3526606a59bcae0b3b850599c71346a30
ALSchwalm <adamschwalm@gmail.com>
2013-12-19 17:41:17 -0600

    Added missing treeGA crossover

---
+++ devolve/tree/crossover.d
@@ -0,0 +1,33 @@
+module devolve.tree.crossover;
+import devolve.tree.generator;
+import std.random;
+import std.stdio;
+
+BaseNode!T singlePoint(T)(ref const BaseNode!T ind1,
+                          ref const BaseNode!T ind2) {
+
+    BaseNode!T newNode = ind1.clone();
+
+    BaseNode!T* currentNew = &newNode;
+    const(BaseNode!T)* currentRight = &ind2;
+    
+    foreach(uint i; 0..ind2.getHeight()) {
+        if (currentRight.getHeight() == currentNew.getHeight() && i != 0 ||
+            !currentNew.getNumChildren() ||
+            !currentRight.getNumChildren()) {
+
+            auto n = currentRight.clone();
+            *currentNew = n;
+            return newNode;
+        }
+        
+        uint choiceNew = uniform(0, currentNew.getNumChildren());
+        uint choiceRight = uniform(0, currentRight.getNumChildren());
+        currentNew = &currentNew.getChild(choiceNew);
+        currentRight = &currentRight.getChild(choiceRight);
+    }
+    
+    return newNode;
+    
+}
+    
