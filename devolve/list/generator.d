39a0890cd47d688a9210890a0f972648c17ae137
Adam Schwalm <adamschwalm@gmail.com>
2014-01-06 14:01:08 -0600

    Fixed bug in assert

---
+++ devolve/list/generator.d
@@ -0,0 +1,27 @@
+module devolve.list.generator;
+import std.random;
+
+/**
+ * Create the initial population by creating 'num' random
+ * allels in the range [low, high).
+ */
+auto randRange(allele : allele[], uint num, allele low, allele high)() {
+    allele[num] ind;
+    foreach (i; 0..num) {
+        ind[i] = cast(allele)(uniform(low, high));
+    }
+    return ind;
+}
+
+/**
+ * Create the initial population using some preset value.
+ */
+auto preset(Alleles...)() {
+    typeof(Alleles[0])[Alleles.length] ind;
+
+    foreach(i, allele; Alleles) {
+        ind[i] = allele;
+    }
+    
+    return ind;
+}
