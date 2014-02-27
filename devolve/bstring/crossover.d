545db7284f80717c63fd9f9dd32c54c6f546e6eb
Adam Schwalm <adamschwalm@gmail.com>
2014-02-27 13:11:03 -0600

    Added twoPoint crossover to bstring

--- devolve/bstring/crossover.d
+++ devolve/bstring/crossover.d
@@ -6,17 +6,17 @@ import std.random;
  * Create a new individual by taking all of the elements
  * from one individual after a randomly chosen point and
  * appending them to all of the values before that point
- * from another individual. Both parents must be of equal
- * length.
+ * from another individual.
  * 
  * ind1 =   1001010110110 $(BR)
  * ind2 =   0010101010010 $(BR)
  * point =  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*
  *
  * child =  1001101010010
+ * NOTE: len will be inferred by the GA
  */
-auto singlePoint(size_t len)(in BitSet!len ind1,
-                             in BitSet!len ind2) {
+auto singlePoint(size_t len)(in BitSet!len parent1,
+                             in BitSet!len parent2) {
 
     auto point = uniform(0, len);
 
@@ -24,10 +24,10 @@ auto singlePoint(size_t len)(in BitSet!len ind1,
 
     foreach(i, ref bit; child) {
         if (i > point) {
-            bit = ind1[i];
+            bit = parent1[i];
         }
         else {
-            bit = ind2[i];
+            bit = parent2[i];
         }
     }
     return child;
@@ -53,3 +53,50 @@ version(unittest) {
     }
 }
 
+/**
+ * Create a new individual by taking all of the alleles
+ * from one individual after a randomly chosen point and
+ * before a different randomly chosen point and
+ * trading them with the alleles from the other individual
+ * which are within that range. 
+ *
+ * ind1 =    0000000000000 $(BR)
+ * ind2 =    1111111111111 $(BR)
+ * point1 =  &nbsp;&nbsp;* $(BR)
+ * point2 =  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*
+ *
+ * result =  0011110000000
+ * NOTE: len will be inferred by the GA
+ */
+auto twoPoint(size_t len)(in BitSet!len parent1,
+                          in BitSet!len parent2) {
+
+    auto point1 = uniform(0, len);
+    auto point2 = uniform(0, len);
+    BitSet!len child;
+
+    foreach(i, ref bit; child) {
+        if (i >= point1 && i < point2) {
+            bit = parent1[i];
+        } else {
+            bit = parent2[i];
+        }
+    }
+
+    return child;
+}
+
+unittest {
+    BitSet!10 a;
+    BitSet!10 b;
+    auto c = twoPoint(a, b);
+
+    BitSet!11 d;
+    assert(!__traits(compiles, twoPoint(a, d)));
+
+    import devolve.bstring;
+    auto ga = new BStringGA!(5, 50, fitness,
+                             generator.random,
+                             selector.topPar!2,
+                             twoPoint);
+}
